e812ad1b90e4835cb62bb90942eab49cce095e17
Andrew Benson <adbenson@cmu.edu>
2015-02-06 23:59:38 -0500

    fix Procfile, debug = True, fix PORT

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 
 # app configuration
 app = Flask(__name__)
-app.debug = False
+app.debug = True
+PORT = int(os.environ.get("PORT", 5000))
 
 # routing
 @app.route("/")
@@ -10,4 +12,4 @@ def home():
   return render_template("index.html")
 
 if __name__ == "__main__":
-  app.run()
+  app.run(port = PORT)
